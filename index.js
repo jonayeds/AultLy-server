e6ed3a2ed31bdd45d20e2ba83a69b424ea827718
jonayeds <jonayeeds@gmail.com>
2024-05-11 17:06:32 +0600

    added recommedation system

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://AultLy:xMZLhxdQJalBTjDj@cluster0.jtcqgec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jtcqgec.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, {
 });
 const database = client.db("AultLy");
 const queryCollection = database.collection("queries");
+const recommendationCollection = database.collection("recommendations");
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -34,8 +35,46 @@ async function run() {
       const result = await queryCollection.insertOne(query)
       res.send(result)
     })
+    
+    app.post('/recommendations', async(req,res)=>{
+      const query = req.body
+      const result = await recommendationCollection.insertOne(query)
+      res.send(result)
+    })
+    app.get('/recommendations', async(req, res)=>{
+      const result = await recommendationCollection.find().toArray()
+      res.send(result)
+    })
     app.get('/queries', async(req, res)=>{
       const result = await queryCollection.find().toArray()
+      res.send(result)
+    })
+    app.get('/queries/:email', async(req, res)=>{
+      const email = req.params.email
+      const result =await queryCollection.find({email}).toArray()
+      res.send(result)
+    })
+    app.get('/queryDetails/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = { _id : new ObjectId(id)}
+      const result = await queryCollection.findOne(query)
+      res.send(result)
+    })
+    app.get('/recommendations/:queryId', async(req, res)=>{
+      const queryId = req.params.queryId
+      const result  = await recommendationCollection.find({queryId}).toArray()
+      res.send(result)
+     })
+    app.put('/queryDetails/:id', async(req , res)=>{
+      const id = req.params.id
+      const query = req.body
+      const filter = {_id : new ObjectId(id)}
+      const updateQuery = {
+        $set : {
+          recommendationCount : parseInt(query.recommendationCount)+1
+        }
+      }
+      const result = await queryCollection.updateOne(filter, updateQuery)
       res.send(result)
     })
 
