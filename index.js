cbf0f66d38941f4057f0a8c4b833bda4720188de
jonayeds <jonayeeds@gmail.com>
2024-05-14 16:45:27 +0600

    handled corse issue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 require('dotenv').config()
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: '*',
     credentials:true
 }))
 app.use(express.json())
